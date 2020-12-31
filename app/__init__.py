fba3feb2cca0d5e18a5335e4ccee993092dab505
Third Uncle <thirduncle@protonmail.com>
2020-12-31 16:32:59 +0200

    End of Chapter 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import  Config
 
 app = Flask(__name__) # variable
+app.config.from_object(Config)
 
 from app import routes # Refers to the app package
--- app/forms.py
+++ app/forms.py
@@ -2,10 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-# A form class defines the fields of the form as class variables i.e. username, password etc.
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
-
